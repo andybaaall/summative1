3513247795f2c2c68424c23dd83d70df5a4700d0
home <home@AndyBaaall.local>
2019-04-10 08:28:25 +1200

    need to finish styling the bottom bar – then MQs, then JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 // console.log("hello world");
 //$(console.log("hello world"));
 
-
 $("#hamburger-button").click(function(){
   $(console.log("got a click"));
+  //toggles the menu
+});
+
+$("#flat-white-button").click(function(){
+  $(console.log("got a click"));
+  //takes us to the default options screen
+});
+
+$("#long-black-button").click(function(){
+  $(console.log("got a click"));
+  //takes us to the options screen with milk greyed out
+  // (there's gotta be a way to do 'if eventTarget was longblack, then adjust the visible options', right?)
+});
+
+$("#scroll-button").click(function(){
+  $(console.log("got a click"));
+  //toggle between scroll-down-button and scroll-up-button
+  //also do the scroll pls
 });
